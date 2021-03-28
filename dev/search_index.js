7261c3839a238b4c8db4dbc4ccdf53e454bd6684
Documenter.jl <documenter@juliadocs.github.io>
2021-03-28 20:11:39 +0000

    build based on 8bdfd10

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PIML","category":"page"},{"location":"#PIML","page":"Home","title":"PIML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PIML]","category":"page"}]
+}
