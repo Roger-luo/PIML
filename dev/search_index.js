21bcad9b1f0f6d7fc7a0b8845c6d9c2862a2c42e
Roger-luo <hiroger@qq.com>
2021-03-28 20:07:23 +0000

    Deploying to gh-pages from  @ 8bdfd1048519c188afe551e5e8bd900733c9b8ba 🚀

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PIML","category":"page"},{"location":"#PIML","page":"Home","title":"PIML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PIML]","category":"page"}]
-}
